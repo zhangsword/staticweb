94af280971fb7364d59d824411563a020a559805
Huan-Xu <huan.xu@ibm.com>
2022-07-04 18:12:56 +0800

    test

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ require("dotenv").config();
   
 // Create Express Server
 const app = express();
-  
+const bodyParser = require('body-parser');
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }))
+app.use(express.json({ limit: '50mb' }));
 // Configuration
 const PORT = 8080;
 const HOST = "0.0.0.0";
@@ -20,13 +23,15 @@ app.use(
     createProxyMiddleware({
         target: API_BASE_URL,
         changeOrigin: true,
-        onProxyReq(proxyReq, req, res, options) {
+        onProxyReq(proxyReq, req, res) {
           if (req.body) {
             const bodyData = JSON.stringify(req.body)
             proxyReq.setHeader('Content-Type', 'application/json')
             proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData))
             proxyReq.write(bodyData)
           }
+          console.log('1==============>', req.body)
+          console.log('2==============>', req.headers)
         },
     })
 );
