89773b46daead710736987d51e2b0b2c9f3e3036
Jian Zhang <sword_zhang@163.com>
2022-07-05 08:17:56 +0800

    fix with simple proxy

--- app.js
+++ app.js
@@ -1,42 +1,26 @@
-const express = require("express");
-const morgan = require("morgan");
-const { createProxyMiddleware } = require("http-proxy-middleware");
-require("dotenv").config();
-  
-// Create Express Server
-const app = express();
-const bodyParser = require('body-parser');
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }))
-app.use(express.json({ limit: '50mb' }));
-// Configuration
-const PORT = 8080;
-const HOST = "0.0.0.0";
-const API_BASE_URL = "https://org21.test.makertown.jp/dtapi";
-  
-// Logging the requests
-app.use(morgan("dev"));
-  
-// Proxy Logic :  Proxy endpoints
-app.use(
-    "/dtapi",
-    createProxyMiddleware({
-        target: API_BASE_URL,
-        changeOrigin: true,
-        onProxyReq(proxyReq, req, res) {
-          if (req.body) {
-            const bodyData = JSON.stringify(req.body)
-            proxyReq.setHeader('Content-Type', 'application/json')
-            proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData))
-            proxyReq.write(bodyData)
-          }
-          console.log('1==============>', req.body)
-          console.log('2==============>', req.headers)
-        },
-    })
-);
-  
-// Starting our Proxy server
-app.listen(PORT, HOST, () => {
-    console.log(`Starting Proxy at ${HOST}:${PORT}`);
-});
+var http = require('http');
+
+http.createServer(onRequest).listen(3000);
+
+function onRequest(client_req, client_res) {
+  console.log('serve: ' + client_req.url);
+
+  var options = {
+    hostname: 'www.google.com',
+    port: 80,
+    path: client_req.url,
+    method: client_req.method,
+    headers: client_req.headers
+  };
+
+  var proxy = http.request(options, function (res) {
+    client_res.writeHead(res.statusCode, res.headers)
+    res.pipe(client_res, {
+      end: true
+    });
+  });
+
+  client_req.pipe(proxy, {
+    end: true
+  });
+}
